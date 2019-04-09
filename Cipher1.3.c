f26803479d5b978840b819bd5fdcca205eedd7a7
Matt Marshall <c3200116@uon.edu.au>
2019-04-09 06:18:46 +0000

    update 10

--- Cipher1.3.c
+++ Cipher1.3.c
@@ -1,6 +1,4 @@
 
-
-
 #include <stdio.h> 
 #include "Functions.h"
 
@@ -20,9 +18,37 @@ int main()
    printf("Enter 1 - 4 followed by <enter>: ");
    scanf("%d", &menu);
    
+   switch (menu){
+    case 1:{ printf("\nInput message to be ENCRYPTED (max 200 characters):\n");
+             scanf(" %[^\n]s", message);   
+             break;}                      //takes a string of input including spaces and assigns to array 'str'
+    case 2:{ printf("\nInput message to be DECRYPTED (max 200 characters):\n");
+             scanf(" %[^\n]s", message); 
+             break;}
+    case 3:{ printf("\nInput message to be ENCRYPTED (max 200 characters):\n");
+             scanf(" %[^\n]s", message); 
+             break;}   
+    case 4:{ printf("\nInput message to be DECRYPTED (max 200 characters):\n");
+             scanf(" %[^\n]s", message); 
+             break;}
+    default: printf("\nInvalid menu selection, please start again.\n");
+    }
+
+
+    for(x=0; message[x]!='\0'; x++){                      //calculates the total number of digits                                         
+         if(message[x]>=0 || message[x]<=127)              //in the string and a
+         max++;
+        }
+    for (count=0; count<=max; count++){    
+        if (message[count]>=97 && message[count]<=122)  //checks for lower case and converts to upper case
+            message[count] = message[count]-32;
+    }
+
+
+
+   
    if (menu==1){
-       printf("\nInput message to be encrypted (max 200 characters):\n");
-       scanf(" %[^\n]s", message);                           //takes a string of input including spaces and assigns to array 'str'
+                       
        
        key=keycheck(key, c);                                  //function which checks the input key is valid
        
@@ -35,17 +61,12 @@ int main()
             if(message[x]>=0 || message[x]<=127)              //in the string and assigns it to 'max'
             max++;
         }
-       //printf("number of digits in string = %d\n", max);   //prints number of digits in string
-                                                                //used for testing only
+       
        
        for (count=0; count<=max; count++){                      //for each digit, checks if it's a letter
            if (message[count]>=65 && message[count]<=90){       //then assigns new value
             message[count] = applyKey(message[count], key);}    //
             
-          else if (message[count]>=97 && message[count]<=122){  //checks for lower case and converts to upper case
-            message[count] = message[count]-32;                 //
-            message[count] = applyKey(message[count], key);     //
-             }
     
       }
      
@@ -57,8 +78,8 @@ int main()
 //DECRYPTION ALGORITHM - SAME AS ABOVE BUT -KEY INSTEAD OF +KEY TO MESSAGE
 
    else if (menu==2){
-       printf("\nInput message to be DEcrypted (max 200 characters):\n");
-       scanf(" %[^\n]s", message);                           //takes a string of input including spaces and assigns to array 'str'
+      // printf("\nInput message to be DEcrypted (max 200 characters):\n");
+      //scanf(" %[^\n]s", message);                           //takes a string of input including spaces and assigns to array 'str'
        
        key=keycheck(key, c);                                  //function which checks the input key is valid
        
@@ -77,10 +98,6 @@ int main()
            if (message[count]>=65 && message[count]<=90){       //then assigns new value
             message[count] = applyKey(message[count], -key);}    //
             
-          else if (message[count]>=97 && message[count]<=122){  //checks for lower case and converts to upper case
-            message[count] = message[count]-32;                 //
-            message[count] = applyKey(message[count], -key);     //
-             }
     
       }
      
@@ -93,8 +110,8 @@ int main()
 //ENCRYPTION WITH SUBSTITUTION..
     else if (menu==3){
     
-    printf("\nInput message to be encrypted (max 200 characters):\n");
-       scanf(" %[^\n]s", message);
+   // printf("\nInput message to be encrypted (max 200 characters):\n");
+   //    scanf(" %[^\n]s", message);
     printf("\nInput substitution key (max 26 characters, no spaces):\n");
        scanf(" %[^\n]s", subs);
        
@@ -112,11 +129,8 @@ int main()
         printf("Incorrect key format, please try again using exactly 26 characters.\n");
         return 0;
     }
-    for (count=0; count<=max; count++){
-        if (message[count]>=97 && message[count]<=122){  //checks for lower case and converts to upper case
-            message[count] = message[count]-32;   
-        }
-    }
+
+        
 
     for (count=0; count<=max; count++){
            switch(message[count]){
